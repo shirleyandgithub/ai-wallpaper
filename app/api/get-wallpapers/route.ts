f59e5072915cf55d3d708e1368015039974c9eb4
shirley <mishayang1991@163.com>
2024-08-16 09:39:51 +0800

    调试限制50条

--- app/api/get-wallpapers/route.ts
+++ app/api/get-wallpapers/route.ts
@@ -2,7 +2,7 @@ import { getWallpapers } from "@/models/wallpaper";
 
 export async function GET(req: Request) {
     console.log("into get wallpapers");
-    const wallpapers = await getWallpapers(1, 1);
+    const wallpapers = await getWallpapers(1, 50);
     console.log("get wallpapers:", wallpapers);
     return Response.json({
         code: 0,
